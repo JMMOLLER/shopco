d5b8c505a0c7a799d05db79a0eeb2c9d65c2a9c3
Jorge Luis Moreno Moller <jlmm.jlmm76@gmail.com>
2025-01-22 15:08:55 -0500

    Paginator component

---
+++ src/components/Paginator/Paginator.ts
@@ -0,0 +1,3 @@
+import Paginator from "./Paginator.astro";
+
+export default Object.assign(Paginator, {});
---
+++ src/components/Paginator/index.ts
@@ -0,0 +1,2 @@
+export type { PaginatorProps as PaginatorType } from '@components/Paginator/Paginator.astro';
+export { default as Paginator } from '@components/Paginator/Paginator';
