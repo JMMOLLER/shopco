ec71b2d1e91a64ec3883a0f1b91ee3be54b05877
Jorge Luis Moreno Moller <jlmm.jlmm76@gmail.com>
2025-01-23 18:44:38 -0500

    Tab component

---
+++ src/components/Tab/Tab.ts
@@ -0,0 +1,4 @@
+import Tab from "./Tab.astro";
+import Panel from "./TabPanel.astro";
+
+export default Object.assign(Tab, { Panel });
---
+++ src/components/Tab/index.ts
@@ -0,0 +1,3 @@
+export type { TabProps as TabType } from '@components/Tab/Tab.astro';
+export type { TabPanelProps as TabPanelType } from '@components/Tab/TabPanel.astro';
+export { default as Tab } from '@components/Tab/Tab';
