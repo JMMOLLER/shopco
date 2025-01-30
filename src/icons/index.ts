a8b751f05605bc26074d84738e46199b1b230816
Jorge Luis Moreno Moller <jlmm.jlmm76@gmail.com>
2025-01-30 17:49:12 -0500

    Implement barrel file to consolidate icon exports

---
+++ src/icons/index.ts
@@ -0,0 +1,47 @@
+import ApplePay from "./ApplePay.astro";
+import CalvinKleinLogo from "./CalvinKleinLogo.astro";
+import Cart from "./Cart.astro";
+import ChevronDown from "./ChevronDown.astro";
+import Facebook from "./Facebook.astro";
+import GitHub from "./GitHub.astro";
+import GooglePay from "./GooglePay.astro";
+import GucciLogo from "./GucciLogo.astro";
+import Instagram from "./Instagram.astro";
+import Mail from "./Mail.astro";
+import MasterCard from "./MasterCard.astro";
+import PayPal from "./PayPal.astro";
+import PradaLogo from "./PradaLogo.astro";
+import Search from "./Search.astro";
+import Starlight from "./Starlight.astro";
+import Twitter from "./Twitter.astro";
+import User from "./User.astro";
+import VersageLogo from "./VersageLogo.astro";
+import Visa from "./Visa.astro";
+import ZaraLogo from "./ZaraLogo.astro";
+
+
+export default Object.assign(
+  {},
+  {
+    ApplePay,
+    CalvinKleinLogo,
+    Cart,
+    ChevronDown,
+    Facebook,
+    GitHub,
+    GooglePay,
+    GucciLogo,
+    Instagram,
+    Mail,
+    MasterCard,
+    PayPal,
+    PradaLogo,
+    Search,
+    Starlight,
+    Twitter,
+    User,
+    VersageLogo,
+    Visa,
+    ZaraLogo
+  }
+);
