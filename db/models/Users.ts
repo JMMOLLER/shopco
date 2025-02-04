0a313396d875dbc422068f40c78ac3dd150838ca
Jorge Luis Moreno Moller <jlmm.jlmm76@gmail.com>
2025-02-03 20:11:14 -0500

    Update Users.ts

--- db/models/Users.ts
+++ db/models/Users.ts
@@ -1,4 +1,4 @@
-import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
+import { text, sqliteTable } from "drizzle-orm/sqlite-core";
 import { sql } from "drizzle-orm";
 
 const UserModel = sqliteTable("user", {
@@ -10,7 +10,7 @@ const UserModel = sqliteTable("user", {
   lastName: text("lastName").notNull(),
   email: text("email").notNull().unique(),
   password: text("password").notNull(),
-  timestamp: text("timestamp")
+  createdAt: text("createdAt")
     .notNull()
     .default(sql`CURRENT_TIMESTAMP`)
 });
