c7a13658cee5778beaafc2b68cad5e5d047798ac
Jorge Luis Moreno Moller <jlmm.jlmm76@gmail.com>
2025-02-03 21:05:52 -0500

    encrypt password

--- src/pages/api/schemas/UserLogin.ts
+++ src/pages/api/schemas/UserLogin.ts
@@ -1,5 +1,11 @@
 import UserRegistration from "./UserRegistration";
+import { z } from "astro/zod";
 
-const UserLogin = UserRegistration.pick({ email: true, password: true });
+/**
+ * @description Extiende el esquema `UserRegistration`, solo conserva el campo `email` y agrega un campo de `password` porque el esquema original encripta la contraseña
+ */
+const UserLogin = UserRegistration.pick({ email: true }).extend({
+  password: z.string().min(8).max(50)
+});
 
 export default UserLogin;
