f1f63a6e7c5dcc7dce0ec6b1e024fa5b8a6ee1d8
Jorge Luis Moreno Moller <jlmm.jlmm76@gmail.com>
2025-03-03 15:15:58 -0500

    loader handler in auth pages

--- src/scripts/auth.ts
+++ src/scripts/auth.ts
@@ -1,10 +1,18 @@
 import ToastMessage from "@libs/ToastMessage";
 
 document.addEventListener("DOMContentLoaded", async () => {
+  processAuthParams();
+  setupBtnClickHandler();
+});
+
+/**
+ * @summary Procesa los parámetros de la URL para mostrar mensajes de error
+ */
+async function processAuthParams() {
   const toast = await ToastMessage.getInstance();
   const params = new URLSearchParams(window.location.search);
   const error = params.get("error");
-  if (!error) return
+  if (!error) return;
 
   switch (error) {
     case "User not found":
@@ -20,4 +28,21 @@ document.addEventListener("DOMContentLoaded", async () => {
       toast.error("An error occurred. Please try again.");
       break;
   }
-});
+}
+
+/**
+ * @summary Agrega un manejador de eventos al botón de submit del formulario
+ */
+function setupBtnClickHandler() {
+  try {
+    const form = document.querySelector("form")!;
+    const button = form.querySelector("button")!;
+    button.addEventListener("click", async (e) => {
+      if (!form.checkValidity()) return;
+      const el = e.currentTarget as HTMLButtonElement;
+      el.ariaBusy = "true";
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
